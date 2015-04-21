97c7ca1650005df1adf5c8e3c4bdef925ee57ea2
Illusions <Illusions@Illusionss-MacBook-Pro.local>
2015-04-21 12:33:57 +0400

    Initial Commit

---
+++ test5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test5
+//
+//  Created by Illusions on 4/21/15.
+//  Copyright (c) 2015 Illusions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test5
+//
+//  Created by Illusions on 4/21/15.
+//  Copyright (c) 2015 Illusions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
