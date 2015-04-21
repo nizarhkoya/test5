91540585e55fcbda330f0ac01f3ce121aa679743
Nizar <nizarhassankoya@gmail.com>
2015-04-21 15:34:56 +0400

    ok to merge

--- test5/AppDelegate.m
+++ test5/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    int k = 0;
     return YES;
 }
 
